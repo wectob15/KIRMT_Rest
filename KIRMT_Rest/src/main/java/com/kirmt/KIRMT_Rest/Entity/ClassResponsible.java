83b25d5710de7bc31b000aefea2727af319e9fd9
wectob15 <wectob15@htlkaindorf.at>
2020-01-07 18:37:54 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Entity/ClassResponsible.java
    #	KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Repository/ClassResponsibleRepository.java

--- KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Entity/ClassResponsible.java
+++ KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Entity/ClassResponsible.java
@@ -4,36 +4,21 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "classresponsible")
-public class ClassResponsible implements Serializable {
-    @Id
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-    private String firstname;
-    private String lastname;
+public class ClassResponsible extends User {
+    private String classname;
 
     public ClassResponsible() {
     }
 
-    public ClassResponsible(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+    public ClassResponsible(String username, String password, String email) {
+        super(username, password, email);
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getClassname() {
+        return classname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setClassname(String classname) {
+        this.classname = classname;
     }
 }
