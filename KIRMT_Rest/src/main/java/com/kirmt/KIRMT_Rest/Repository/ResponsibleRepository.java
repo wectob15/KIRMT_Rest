ad4ccf624c8e524c8214427237ba495869ed39fc
wectob15 <wectob15@htlkaindorf.at>
2020-02-05 16:16:14 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Entity/ClassResponsible.java
    #	KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Repository/ClassResponsibleRepository.java

--- KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Repository/ResponsibleRepository.java
+++ KIRMT_Rest/src/main/java/com/kirmt/KIRMT_Rest/Repository/ResponsibleRepository.java
@@ -1,10 +1,10 @@
 package com.kirmt.KIRMT_Rest.Repository;
 
-import com.kirmt.KIRMT_Rest.Entity.ClassResponsible;
+import com.kirmt.KIRMT_Rest.Entity.Responsible;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClassResponsibleRepository extends JpaRepository<ClassResponsible, Integer> {
+public interface ResponsibleRepository extends JpaRepository<Responsible, Integer> {
 
 }
